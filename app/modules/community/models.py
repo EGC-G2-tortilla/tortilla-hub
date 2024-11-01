45c54895373ffe8ec1acb460ef2e0a4de52c27f5
DelfinSR <delsanrub@alum.us.es>
2024-11-01 21:20:28 +0100

    feat: added name, url, description to community model

--- app/modules/community/models.py
+++ app/modules/community/models.py
@@ -3,6 +3,11 @@ from app import db
 
 class Community(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    # TODO: add tags
+    # TODO: add users
 
     def __repr__(self):
         return f'Community<{self.id}>'
