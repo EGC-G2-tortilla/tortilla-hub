b7144558d40321daa46e1467ba4fa86eb2374ab6
DelfinSR <delsanrub@alum.us.es>
2024-11-01 21:24:12 +0100

    feat: community form created(not finished)

--- app/modules/community/forms.py
+++ app/modules/community/forms.py
@@ -1,6 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class CommunityForm(FlaskForm):
+    name = StringField('Title', validators=[DataRequired(), Length(max=256)])
+    description = TextAreaField('Description', validators=[DataRequired()])
+    url = TextAreaField('URL', validators=[DataRequired()])
+
+    # TODO: add tags
+    # TODO: add users
+
     submit = SubmitField('Save community')
