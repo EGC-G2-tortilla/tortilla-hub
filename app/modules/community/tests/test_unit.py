ad16e36e1b2c2e5f038ee9286471d84c1c21beae
DelfinSR <delsanrub@alum.us.es>
2024-11-09 16:24:46 +0100

    test: añadido test para community

--- app/modules/community/tests/test_unit.py
+++ app/modules/community/tests/test_unit.py
@@ -1,5 +1,10 @@
 import pytest
 
+from app import db
+from app.modules.conftest import login, logout
+from app.modules.auth.models import User
+from app.modules.community.models import Community
+from app.modules.community.models import user_community_Table
 
 @pytest.fixture(scope='module')
 def test_client(test_client):
@@ -7,10 +12,15 @@ def test_client(test_client):
     Extends the test_client fixture to add additional specific data for module testing.
     """
     with test_client.application.app_context():
-        # Add HERE new elements to the database that you want to exist in the test context.
-        # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
-
+        user_test = User(email='user@example.com', password='test1234')
+        db.session.add(user_test)
+        db.session.commit()
+        
+        community_test = Community(name="sample community", url="https://sample.com", description="sample sample sample")
+        community_test.members.append(user_test)
+        db.session.add(community_test)
+        db.session.commit()
+        
     yield test_client
 
 
@@ -20,5 +30,11 @@ def test_sample_assertion(test_client):
     It does not communicate with the Flask application; it only performs a simple assertion to
     confirm that the tests in this module can be executed.
     """
-    greeting = "Hello, World!"
-    assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
+    test_user = User.query.filter_by(email='user@example.com').first()
+    test_community = Community.query.filter_by(name="sample community").first()
+    
+    assert test_community.name == "sample community"
+    assert test_community.url == "https://sample.com" 
+    assert test_community.description == "sample sample sample"
+    assert test_user in test_community.members
+
