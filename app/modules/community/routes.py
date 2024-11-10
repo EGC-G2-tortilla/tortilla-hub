ce20af6b77eb3d22feca88461d9d3d60db16fad0
DelfinSR <delsanrub@alum.us.es>
2024-11-10 20:21:30 +0100

    feat: añadida vista de descripción de comunidad y botones para acceder a ella

--- app/modules/community/routes.py
+++ app/modules/community/routes.py
@@ -1,29 +1,53 @@
 import logging
 
-from flask import render_template
+from flask import (
+    render_template,
+    abort
+)
 
 from app.modules.community.services import CommunityService
+from app.modules.dataset.services import DataSetService
 from app.modules.community import community_bp
 
 logger = logging.getLogger(__name__)
+
+community_service = CommunityService()
+dataset_service = DataSetService()
 
 
 @community_bp.route('/community', methods=['GET'])
 def index():
     logger.info("Access community index")
-    community_service = CommunityService()
 
-
-    # Statistics: total size of all datasets
-    # TODO: intentar que se haga solo con los datasets sincronizados por que no sabemos como son los otros
     communities = community_service.get_all_communities()
-    #total_size = sum(dataset.get_file_total_size() for dataset in total_datasets)
-    #total_size = SizeService().get_human_readable_size(total_size)
 
     return render_template(
         "community/index.html",
 
         communities=communities
     )
-    
 
+
+@community_bp.route("/community/<string:community_name>/", methods=["GET"])
+def get_community_by_name(community_name):
+    community = community_service.get_community_by_name(community_name)
+
+    if not community:
+        abort(404)
+
+    datasets = dataset_service.get_by_community_id(community.id)
+
+    return render_template("community/community.html",
+                           community=community,
+                           datasets=datasets)
+
+
+@community_bp.route("/community/<string:community_name>/info", methods=["GET"])
+def get_community_info_by_name(community_name):
+    community = community_service.get_community_by_name(community_name)
+
+    if not community:
+        abort(404)
+
+    return render_template("community/community_info.html",
+                           community=community)
