bdd071382d73c4858a95884ba0602b0222322ff1
DelfinSR <delsanrub@alum.us.es>
2024-11-10 15:13:28 +0100

    feat: Primera vista de comunidades funcionando. Se pueden listar las comunidades.

--- app/modules/community/routes.py
+++ app/modules/community/routes.py
@@ -2,8 +2,7 @@ import logging
 
 from flask import render_template
 
-from app.modules.featuremodel.services import FeatureModelService
-from app.modules.dataset.services import DataSetService, SizeService
+from app.modules.community.services import CommunityService
 from app.modules.community import community_bp
 
 logger = logging.getLogger(__name__)
@@ -11,37 +10,20 @@ logger = logging.getLogger(__name__)
 
 @community_bp.route('/community', methods=['GET'])
 def index():
-    logger.info("Access index")
-    dataset_service = DataSetService()
-    feature_model_service = FeatureModelService()
+    logger.info("Access community index")
+    community_service = CommunityService()
 
-    # Statistics: total datasets and feature models
-    datasets_counter = dataset_service.count_synchronized_datasets()
-    feature_models_counter = feature_model_service.count_feature_models()
-
-    # Statistics: total downloads
-    total_dataset_downloads = dataset_service.total_dataset_downloads()
-    total_feature_model_downloads = feature_model_service.total_feature_model_downloads()
-
-    # Statistics: total views
-    total_dataset_views = dataset_service.total_dataset_views()
-    total_feature_model_views = feature_model_service.total_feature_model_views()
 
     # Statistics: total size of all datasets
     # TODO: intentar que se haga solo con los datasets sincronizados por que no sabemos como son los otros
-    total_datasets = dataset_service.get_all_datasets()
-    total_size = sum(dataset.get_file_total_size() for dataset in total_datasets)
-    total_size = SizeService().get_human_readable_size(total_size)
+    communities = community_service.get_all_communities()
+    #total_size = sum(dataset.get_file_total_size() for dataset in total_datasets)
+    #total_size = SizeService().get_human_readable_size(total_size)
 
     return render_template(
         "community/index.html",
-        datasets=dataset_service.latest_synchronized(),
-        datasets_counter=datasets_counter,
-        feature_models_counter=feature_models_counter,
-        total_dataset_downloads=total_dataset_downloads,
-        total_feature_model_downloads=total_feature_model_downloads,
-        total_dataset_views=total_dataset_views,
-        total_feature_model_views=total_feature_model_views,
 
-        total_dataset_size=total_size
+        communities=communities
     )
+    
+
--- app/modules/community/services.py
+++ app/modules/community/services.py
@@ -5,3 +5,6 @@ from core.services.BaseService import BaseService
 class CommunityService(BaseService):
     def __init__(self):
         super().__init__(CommunityRepository())
+        
+    def get_all_communities(self):
+        return self.repository.get_all_communities()
