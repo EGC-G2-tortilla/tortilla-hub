9463233f2e10b1a65459a0e0048aba21ef6b77cb
DelfinSR <delsanrub@alum.us.es>
2024-11-11 22:55:09 +0100

    test: añadidos tests unitarios para comprobar la funcionalidad del servicio de unirse a una comunidad

--- app/modules/community/tests/test_unit.py
+++ app/modules/community/tests/test_unit.py
@@ -3,6 +3,9 @@ import pytest
 from app import db
 from app.modules.auth.models import User
 from app.modules.community.models import Community
+from app.modules.community.services import CommunityService
+
+service = CommunityService()
 
 
 @pytest.fixture(scope='module')
@@ -13,9 +16,11 @@ def test_client(test_client):
     with test_client.application.app_context():
         user_test = User(email='user@example.com', password='test1234')
         db.session.add(user_test)
+        user_test2 = User(email='user_1@example.com', password='test1234')
+        db.session.add(user_test2)
         db.session.commit()
 
-        community_test = Community(name="sample community",
+        community_test = Community(name="sample community", admin=user_test.id,
                                    url="https://sample.com", description="sample sample sample")
         community_test.members.append(user_test)
         db.session.add(community_test)
@@ -37,3 +42,22 @@ def test_sample_assertion(test_client):
     assert test_community.url == "https://sample.com"
     assert test_community.description == "sample sample sample"
     assert test_user in test_community.members
+
+
+def test_join_community_and_is_user_in_community(test_client):
+    test_user = User.query.filter_by(email='user_1@example.com').first()
+    test_community = Community.query.filter_by(name="sample community").first()
+
+    service.join_a_community(test_user, test_community)
+    test_community = Community.query.filter_by(name="sample community").first()
+
+    assert test_user in test_community.members
+
+
+def test_is_user_in_community(test_client):
+    test_user = User.query.filter_by(email='user_1@example.com').first()
+    test_community = Community.query.filter_by(name="sample community").first()
+
+    result = service.is_user_in_community(test_user, test_community)
+
+    assert result
