103df8b852e3c5040f1324ec7be94b285f202f4f
DelfinSR <delsanrub@alum.us.es>
2024-11-28 19:59:33 +0100

    feat: creado modelo para la solicitud de unirse a una comunidad

--- app/modules/community_join_request/models.py
+++ app/modules/community_join_request/models.py
@@ -3,6 +3,8 @@ from app import db
 
 class CommunityJoinRequest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_who_wants_to_join_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey("community.id"), nullable=True)
 
     def __repr__(self):
         return f'CommunityJoinRequest<{self.id}>'
