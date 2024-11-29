914abb85d1611e34dafe11647f4724461003949c
DelfinSR <delsanrub@alum.us.es>
2024-11-29 19:42:01 +0100

    feat: repositorio de CommunityJoinRequest actualizado

--- app/modules/community_join_request/repositories.py
+++ app/modules/community_join_request/repositories.py
@@ -5,3 +5,6 @@ from core.repositories.BaseRepository import BaseRepository
 class CommunityJoinRequestRepository(BaseRepository):
     def __init__(self):
         super().__init__(CommunityJoinRequest)
+
+    def get_request_by_id(self, request_id: int) -> CommunityJoinRequest:
+        return self.model.query.filter_by(request_id=request_id)
