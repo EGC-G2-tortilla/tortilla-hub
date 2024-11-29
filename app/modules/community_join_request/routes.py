e923ad4cd0c5e2580f866ecd63a75794bbffb52d
DelfinSR <delsanrub@alum.us.es>
2024-11-29 20:09:42 +0100

    feat: actualizadas rutas de CommunityJoinRequestService para que se pueda crear, aceptar y eliminar una solicitud para unirse a una comunidad

--- app/modules/community_join_request/routes.py
+++ app/modules/community_join_request/routes.py
@@ -1,7 +1,62 @@
-from flask import render_template
+from flask import abort, redirect
+from flask_login import login_required, current_user
+from app.modules.community.services import CommunityService
+from app.modules.community_join_request.services import CommunityJoinRequestService
 from app.modules.community_join_request import community_join_request_bp
 
+community_service = CommunityService()
+community_join_request_service = CommunityJoinRequestService()
 
-@community_join_request_bp.route('/community_join_request', methods=['GET'])
-def index():
-    return render_template('community_join_request/index.html')
+
+@community_join_request_bp.route("/community/<string:community_name>/join", methods=["POST"])
+@login_required
+def join_community(community_name):
+    community = community_service.get_community_by_name(community_name)
+
+    if not community:
+        abort(404)
+
+    community_join_request_service.create_from_request(current_user, community)
+    return redirect("/community/"+community_name)
+
+
+@community_join_request_bp.route("/community-join-request/<int:request_id>/accept", methods=["POST"])
+@login_required
+def accept_request(request_id):
+    request_join = community_join_request_service.get_request_by_id(request_id)
+
+    if not request_join:
+        abort(404)
+
+    community = community_service.get_community_by_id(request.community_id)
+
+    if not community:
+        abort(404)
+
+    if community.admin != current_user.id:
+        abort(503)
+
+    community_join_request_service.accept_request(current_user, community, request_join)
+
+    return redirect("/community/"+community.name)
+
+
+@community_join_request_bp.route("/community-join-request/<int:request_id>/decline", methods=["POST"])
+@login_required
+def decline_request(request_id):
+    request_join = community_join_request_service.get_request_by_id(request_id)
+
+    if not request_join:
+        abort(404)
+
+    community = community_service.get_community_by_id(request_join.community_id)
+
+    if not community:
+        abort(404)
+
+    if community.admin != current_user.id:
+        abort(503)
+
+    community_join_request_service.decline_request(request_join)
+
+    return redirect("/community/"+community.name)
--- app/modules/community_join_request/services.py
+++ app/modules/community_join_request/services.py
@@ -1,7 +1,23 @@
 from app.modules.community_join_request.repositories import CommunityJoinRequestRepository
+from app.modules.community.services import CommunityService
+from app.modules.community_join_request.models import CommunityJoinRequest
 from core.services.BaseService import BaseService
 
 
 class CommunityJoinRequestService(BaseService):
     def __init__(self):
         super().__init__(CommunityJoinRequestRepository())
+
+    def create_from_request(self, current_user, community) -> CommunityJoinRequest:
+        self.create(user_who_wants_to_join_id=current_user.id,
+                    community_id=community.id)
+
+    def get_request_by_id(self, request_id: int) -> CommunityJoinRequest:
+        return self.get_request_by_id(request_id)
+
+    def accept_request(self, current_user, community, request):
+        CommunityService.join_a_community(current_user, community)
+        self.delete(request)
+
+    def decline_request(self, request):
+        self.delete(request)
