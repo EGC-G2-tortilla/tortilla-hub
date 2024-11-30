6fe98897d9bffd6431a0fbea1a6aa47ed3ad0479
danvelcam <danvelcam@alum.us.es>
2024-11-30 22:59:05 +0100

    feat: añadidos los tests de carga para las operaciones de staging area closes #104

--- app/modules/dataset/tests/locustfile.py
+++ app/modules/dataset/tests/locustfile.py
@@ -11,8 +11,31 @@ class DatasetBehavior(TaskSet):
     def dataset(self):
         response = self.client.get("/dataset/upload")
         get_csrf_token(response)
+    
+    @task
+    def dataset_stage(self):
+        response = self.client.post("/dataset/stage/246")
+        get_csrf_token(response)
+    
+    @task
+    def dataset_unstage(self):
+        response = self.client.post("/dataset/unstage/246")
+        get_csrf_token(response)
 
+    @task
+    def stage_all(self):
+        response = self.client.post("/dataset/stage/all")
+        get_csrf_token(response)
+    
+    @task
+    def unstage_all(self):
+        response = self.client.post("/dataset/unstage/all")
+        get_csrf_token(response)
 
+    @task
+    def publish_dataset(self):
+        response = self.client.post("/dataset/publish")
+        get_csrf_token(response)
 class DatasetUser(HttpUser):
     tasks = [DatasetBehavior]
     min_wait = 5000
