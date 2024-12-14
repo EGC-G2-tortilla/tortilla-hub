a0cba15f78b99a7124c61bbb224e51b76121b17e
Daniel Fernandez Caballero <danfercab@alum.us.es>
2024-12-14 11:35:57 +0100

    update: testing unitario Discord-bot terminado
    Añadir tests sobre los comandos de Discord /help, /most_downlodad, /datasets_counter, /feature_models_counter, /total_datasets_downloads
    
    Closes #218

--- app/modules/discord_bot/tests/test_unit.py
+++ app/modules/discord_bot/tests/test_unit.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import AsyncMock, patch
 from app.modules.discord_bot.bot import (
+    help,
     most_downloaded,
     total_dataset_downloads,
     datasets_counter,
@@ -8,16 +9,37 @@ from app.modules.discord_bot.bot import (
 )
 
 
+@patch("app.modules.discord_bot.bot.help")
+@pytest.mark.asyncio
+async def test_help_command(mock_help):
+
+    interaction_mock = AsyncMock()
+    interaction_mock.response.send_message = AsyncMock()
+
+    await help(interaction_mock)
+    # Verifica el contenido del embed
+    embed = interaction_mock.response.send_message.call_args[1]["embed"]
+    assert embed.title == "📖 Ayuda del Bot"
+    assert "Aquí tienes la lista de comandos disponibles para interactuar con este bot." in embed.description
+    assert len(embed.fields) == 4
+
+    for field in embed.fields:
+        assert field.name in ["📂 Gestión de Datasets", "📊 Estadísticas Generales",
+                              "🔧 Modelos de Características", "📚 Autores"]
+        assert field.value != ""
+
+
 # Test for most_downloaded_datasets
 @patch("app.modules.discord_bot.bot.dataset_service")
 @pytest.mark.asyncio
 async def test_most_downloaded_datasets(mock_dataset_service):
-    mock_dataset_service.most_downloaded.return_value = [
+    mock_datasets = [
         {"name": "dataset1", "downloads": 10},
         {"name": "dataset2", "downloads": 5},
         {"name": "dataset3", "downloads": 3},
         {"name": "dataset4", "downloads": 1},
     ]
+    mock_dataset_service.most_downloaded.return_value = mock_datasets
     interaction_mock = AsyncMock()
     interaction_mock.response.send_message = AsyncMock()
 
@@ -27,24 +49,9 @@ async def test_most_downloaded_datasets(mock_dataset_service):
     interaction_mock.response.send_message.assert_called_once()
     sent_message = interaction_mock.response.send_message.call_args[1]
     assert "Gráfico de descargas" in sent_message["embed"].title
-    
-
-# Test for total_dataset_downloads
-@patch("app.modules.discord_bot.bot.dataset_service")
-@pytest.mark.asyncio
-async def test_total_dataset_downloads(mock_dataset_service):
-    mock_dataset_service.total_dataset_downloads.return_value = 150
-
-    interaction_mock = AsyncMock()
-    interaction_mock.response.send_message = AsyncMock()
-
-    await total_dataset_downloads(interaction_mock)
-
-    mock_dataset_service.total_dataset_downloads.assert_called_once()
-    interaction_mock.response.send_message.assert_called_once()
-    sent_message = interaction_mock.response.send_message.call_args[1]
-    assert "Total de Descargas de Datasets" in sent_message["embed"].title
-    assert "150" in sent_message["embed"].description
+    # Validar que el archivo del gráfico fue adjuntado correctamente
+    file = interaction_mock.response.send_message.call_args[1]["file"]
+    assert file.filename == "top_datasets.png"
 
 
 # Test for datasets_counter
@@ -82,3 +89,20 @@ async def test_feature_models_counter(mock_feature_model_service):
     interaction_mock.response.send_message.assert_called_once()
     assert "10" in sent_message["embed"].description
 
+
+# Test for total_dataset_downloads
+@patch("app.modules.discord_bot.bot.dataset_service")
+@pytest.mark.asyncio
+async def test_total_dataset_downloads(mock_dataset_service):
+    mock_dataset_service.total_dataset_downloads.return_value = 150
+
+    interaction_mock = AsyncMock()
+    interaction_mock.response.send_message = AsyncMock()
+
+    await total_dataset_downloads(interaction_mock)
+
+    mock_dataset_service.total_dataset_downloads.assert_called_once()
+    interaction_mock.response.send_message.assert_called_once()
+    sent_message = interaction_mock.response.send_message.call_args[1]
+    assert "Total de Descargas de Datasets" in sent_message["embed"].title
+    assert "150" in sent_message["embed"].description
