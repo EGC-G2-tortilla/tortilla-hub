d113d8fb951eb7987b3cb2d062867da4e2d69906
Manuel Barcia Jimenez <manbarjim2@gmail.com>
2024-12-14 14:54:04 +0100

    fix: Error con models no usado

--- app/modules/flamapy/repositories.py
+++ app/modules/flamapy/repositories.py
@@ -1,7 +1,6 @@
-from app.modules.flamapy.models import Flamapy
 from core.repositories.BaseRepository import BaseRepository
 
 
 class FlamapyRepository(BaseRepository):
     def __init__(self):
-        super().__init__(Flamapy)
+        super().__init__()
