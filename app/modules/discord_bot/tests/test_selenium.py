7244fddb90e894a8ee107f6da65a118c89fca44c
Daniel Fernandez Caballero <danfercab@alum.us.es>
2024-12-15 18:31:14 +0100

    test: Testing completo de Selenium discord-bot
    Closes #246
    
    Co-authored-by: Ramón Gavira <ramgavsan@alum.us.es>

--- app/modules/discord_bot/tests/test_selenium.py
+++ app/modules/discord_bot/tests/test_selenium.py
@@ -3,17 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv
 from core.selenium.common import initialize_driver, close_driver
 import os
 
-# Cargar las variables de entorno
 load_dotenv()
 
 # Obtener las credenciales de Discord desde el archivo .env
-username = os.getenv("DISCORD_USERNAME")
-password = os.getenv("DISCORD_PASSWORD")
+username = os.getenv("CREDENTIALS_USERNAME")
+password = os.getenv("CREDENTIALS_PASSWORD")
 
 
 def wait_for_page_to_load(driver, timeout=10):
@@ -25,9 +23,16 @@ def wait_for_page_to_load(driver, timeout=10):
     )
 
 
-def test_discord_bot_command_help():
+def scroll_to_top(driver):
     """
-    Test para verificar que el bot responde correctamente a un slash command help en Discord.
+    Desplaza hacia arriba en la página utilizando JavaScript.
+    """
+    driver.execute_script("window.scrollTo(0, 0);")
+
+
+def test_discord_bot_commands():
+    """
+    Test para verificar que el bot responde correctamente a los slash commands en Discord.
     """
     driver = initialize_driver()
 
@@ -49,65 +54,107 @@ def test_discord_bot_command_help():
 
         time.sleep(10)
 
-        server_url = "https://discord.com/channels/1313534413082071140/1317455188025413643"
+        server_url = "https://discord.com/channels/1317900109542985738/1317900109542985741"
         driver.get(server_url)
         wait_for_page_to_load(driver)
 
         if len(driver.window_handles) == 0:
             raise Exception("La ventana se ha cerrado antes de completar el test.")
 
-        # Paso 4: Encontrar el campo de entrada para enviar un comando
-        message_box = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@aria-label='Enviar mensaje a #testing']"))
-        )
-
-        slash_command = "/help"
-        message_box.send_keys(slash_command)
-        message_box.send_keys(Keys.RETURN)
-        message_box.send_keys(Keys.RETURN)
-
-        response_message = WebDriverWait(driver, 10).until(
+        # Crear el canal de prueba
+        create_channel_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((
                 By.XPATH,
-                "//div[@class='embedTitle_b0068a embedMargin_b0068a']//span[contains(text(), 'Ayuda del Bot')]"
+                "//button[@class='addButton_a08117 forceVisible_a08117 button_dd4f85 lookBlank_dd4f85 "
+                "colorBrand_dd4f85 grow_dd4f85']"
             ))
         )
-        print("Bot respondió correctamente:", response_message.text)
 
-        # actions = ActionChains(driver)
-        # actions.move_to_element(response_message).perform()
+        create_channel_button.click()
 
-        # # Paso 7: Esperar y hacer clic en el botón de tres puntos
-        # try:
-        #     three_dots_button = WebDriverWait(driver, 10).until(
-        #         EC.element_to_be_clickable((
-        #             By.XPATH,
-        #             "//div[@class='hoverBarButton_e986d9 button_f7e168 selected_f7e168']"
-        #         ))
-        #     )
-        #     three_dots_button.click()
-        # except Exception as e:
-        #     print("Error al encontrar o hacer clic en el botón de tres puntos:", e)
+        channel_name_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                "//input[@class='inputDefault_f8bc55 input_f8bc55 inputInner_b545d5']"
+            ))
+        )
 
-        # # Paso 8: Esperar y hacer clic en "Eliminar"
-        # delete_button = WebDriverWait(driver, 10).until(
-        #     EC.element_to_be_clickable((
-        #         By.XPATH,
-        #         "//button[@aria-label='Eliminar']"
-        #     ))
-        # )
-        # delete_button.click()
+        channel_name_field.send_keys("testing")
+        channel_name_field.send_keys(Keys.RETURN)
 
-        # time.sleep(2)  # Esperar a que aparezca
+        wait_for_page_to_load(driver)
+        time.sleep(3)
 
-        # # Paso 10: Confirmar la eliminación del mensaje
-        # confirm_delete_button = WebDriverWait(driver, 10).until(
-        #     EC.presence_of_element_located((
-        #         By.XPATH,
-        #         "//button[@type='submit' and contains(text(), 'Eliminar')]"
-        #     ))
-        # )
-        # confirm_delete_button.click()
+        message_box = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                "//div[@class='markup_f8f345 editor_a552a6 slateTextArea_e52116 fontSize16Padding_bdf0de']"
+            ))
+        )
+
+        commands = [
+            ("/help", "Ayuda del Bot"),
+            ("/datasets_counter", "Total de Datasets Sincronizados"),
+            ("/feature_models_counter", "Total de Modelos de Características"),
+            ("/total_dataset_downloads", "Total de Descargas de Datasets"),
+            ("/total_feature_model_downloads", "Total de descargas de modelos de características"),
+            ("/total_dataset_views", "Total de Descargas de Datasets"),
+            ("/total_feature_model_views", "Total de Vistas de Modelos de Características"),
+            ("/datasets", "Dataset:"),
+            ("/most_popular_authors", "Autores Más Populares")
+        ]
+
+        for command, expected_text in commands:
+            message_box.send_keys(command)
+            time.sleep(0.5)
+            message_box.send_keys(Keys.RETURN)
+            time.sleep(0.5)
+            message_box.send_keys(Keys.RETURN)
+
+            response_message = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((
+                    By.XPATH,
+                    f"//div[@class='embedTitle_b0068a embedMargin_b0068a']//span[contains(text(), '{expected_text}')]"
+                ))
+            )
+            print(f"Bot respondió correctamente: {response_message.text}")
+
+            time.sleep(2)
+
+        scroll_to_top(driver)
+
+        options_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                "//button[@class='button_ccfa44 button_dd4f85 lookBlank_dd4f85 colorBrand_dd4f85 sizeMin_dd4f85 "
+                "grow_dd4f85']"
+            ))
+        )
+        options_button.click()
+        wait_for_page_to_load(driver)
+
+        delete_channel_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                "//div[@class='item_a0 themed_a0'][@aria-label='Eliminar canal']"
+            ))
+            or EC.presence_of_element_located((
+                By.XPATH,
+                "//div[@class='item_a0 themed_a0'][@aria-label='Delete channel']"
+            ))
+        )
+        delete_channel_button.click()
+        wait_for_page_to_load(driver)
+
+        confirm_delete_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((
+                By.XPATH,
+                "//button[@class='button_dd4f85 lookFilled_dd4f85 colorRed_dd4f85 sizeMedium_dd4f85 grow_dd4f85']"
+            ))
+        )
+
+        confirm_delete_button.click()
+        time.sleep(2)
 
     except Exception as e:
         print(f"Se produjo un error: {str(e)}")
@@ -116,6 +163,5 @@ def test_discord_bot_command_help():
         close_driver(driver)
 
 
-# Ejecutar el test
 if __name__ == "__main__":
-    test_discord_bot_command_help()
+    test_discord_bot_commands()
